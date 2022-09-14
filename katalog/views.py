3b5fd06b1d10f24d8237bf87ab9f8bb38374b522
fideraphael <fideraphael@gmail.com>
2022-09-14 20:23:29 +0700

    commit

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
+
+def show_catalog(request):
+    data_katalog_item = CatalogItem.objects.all()
+    context = {
+        'list_item': data_katalog_item,
+        'name': 'Raphael',
+        'StudentID':'2106720986'
+    }
+    return render(request, "katalog.html", context)
 # TODO: Create your views here.
