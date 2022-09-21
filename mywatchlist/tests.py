59ec916a737f975c83e113478186ca1b15aab20d
fideraphael <fideraphael@gmail.com>
2022-09-21 21:38:52 +0700

    last edit

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -2,6 +2,6 @@ from django.test import TestCase
 
 class HomepageTests(TestCase):
     def test_url_exists_at_correct_location(self):
-        response = self.client.get("/mywatchlist")
+        response = self.client.get("/mywatchlist/html/")
         self.assertEqual(response.status_code, 200)
 # Create your tests here.
